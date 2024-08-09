936fcb84b58bc369a056ec21d583a4e2b34f9423
Muhammad Suleman <muhammadsulemanlahore@gmail.com>
2024-08-09 11:31:29 +0500

    init

---
+++ learn-nextjs-2/src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Homepage() {
+  return (
+    <h1 className="text-center p-20 hover:text-yellow-600">Hello, I am Muhammad Suleman and this is my first Next.js project.</h1>
+  );
+}
